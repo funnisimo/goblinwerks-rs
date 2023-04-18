62f41d30fd82982279552cf6b083b4b0784bcfc9
Funnisimo <scastette@yahoo.com>
2023-04-18 12:20:45 -0500

    added some system macro cases

--- crates/gw_macro/src/system.rs
+++ crates/gw_macro/src/system.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::parse_macro_input;
-use syn::ItemFn;
+use syn::{parse_macro_input, Type};
+use syn::{FnArg, ItemFn};
 
 pub fn make_system_fn(_args: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
@@ -11,16 +11,25 @@ pub fn make_system_fn(_args: TokenStream, item: TokenStream) -> TokenStream {
 
     println!("=========== {} =============", sys_fn_ident);
 
+    let mut arg_types: Vec<Box<Type>> = Vec::new();
     for arg in input_fn.sig.inputs.iter() {
-        println!("arg = {:?}", arg);
+        match arg {
+            FnArg::Typed(t) => {
+                println!("Typed arg type = {:?}", t.ty);
+                arg_types.push(t.ty.clone());
+            }
+            FnArg::Receiver(r) => println!("Receiver Arg = {:?}", r),
+        }
     }
+
+    let i = (0..arg_types.len()).map(syn::Index::from);
 
     quote! {
         #input_fn
 
         fn #sys_fn_ident(ecs: &Ecs) {
-            let data = <(&Ecs,)>::fetch(ecs);
-            #fn_ident(data.0);
+            let data = <(#(#arg_types),*,)>::fetch(ecs);
+            #fn_ident(#(data.#i ),*);
         }
     }
     .into()
--- crates/gw_macro/tests/basic.rs
+++ crates/gw_macro/tests/basic.rs
@@ -1,4 +1,4 @@
-use gw_ecs::{Ecs, Fetch};
+use gw_ecs::{Ecs, Fetch, GlobalMut, TryGlobal};
 use gw_macro::system;
 
 struct Count(u32);
@@ -26,5 +26,45 @@ fn basic_ecs() {
     {
         let count = ecs.get_global::<Count>().unwrap();
         assert_eq!(count.0, 2);
+    }
+}
+
+#[system]
+fn increment_global(mut global: GlobalMut<Count>) {
+    global.0 += 1;
+}
+
+#[test]
+fn basic_global() {
+    let mut ecs = Ecs::new();
+    ecs.insert_global(Count(0));
+
+    increment_global_system(&ecs);
+
+    {
+        let count = ecs.get_global::<Count>().unwrap();
+        assert_eq!(count.0, 1);
+    }
+}
+
+struct Age(u32);
+
+#[system]
+fn try_double(try_age: TryGlobal<Age>, mut count: GlobalMut<Count>) {
+    if try_age.is_none() {
+        count.0 += 1;
+    }
+}
+
+#[test]
+fn basic_double() {
+    let mut ecs = Ecs::new();
+    ecs.insert_global(Count(0));
+
+    try_double_system(&ecs);
+
+    {
+        let count = ecs.get_global::<Count>().unwrap();
+        assert_eq!(count.0, 1);
     }
 }
