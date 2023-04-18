df5164f586e21349dfa38916c3ebe6359bef3571
Funnisimo <scastette@yahoo.com>
2023-04-18 13:20:31 -0500

    removed borrow

--- crates/gw_ecs/src/lib.rs
+++ crates/gw_ecs/src/lib.rs
@@ -1,24 +1,17 @@
-// pub mod entities;
-// mod internal;
-
-// pub mod borrow;
 mod component;
 mod ecs;
 mod entity;
+pub mod fetch;
 mod level;
 mod levels;
 pub mod refcell;
 pub mod resource;
 pub mod storage;
 pub mod system;
-// mod view;
-pub mod fetch;
 
-// pub use borrow::*;
 pub use component::Component;
 pub use ecs::*;
 pub use entity::Entity;
 pub use fetch::*;
 pub use level::*;
 pub use levels::*;
-// pub use view::*;
