2694e681bb34cf3b104bf4cd3318adb17eb93d54
Funnisimo <scastette@yahoo.com>
2023-06-03 16:07:43 -0500

    renamed components -> meta

--- crates/gw_bevy/src/components/mod.rs
+++ crates/gw_bevy/src/components/mod.rs
@@ -1,9 +1,9 @@
 mod component;
 mod component_set;
-mod components;
+mod meta;
 mod system_param;
 
 pub use component::*;
 pub use component_set::*;
-pub use components::*;
+pub use meta::*;
 pub use system_param::*;
